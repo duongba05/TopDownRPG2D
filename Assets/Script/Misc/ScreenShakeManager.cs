d742650c59dfbcd946011571b1ac74d36dd2c4e1
pesduo <pesduo05@gmail.com>
2025-03-04 22:07:12 +0700

    ScreenShake

---
+++ Assets/Script/Misc/ScreenShakeManager.cs
@@ -0,0 +1,17 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class ScreenShakeManager : Singleton<ScreenShakeManager>
+{
+    private CinemachineImpulseSource source;
+    protected override void Awake()
+    {
+        base.Awake();
+        source = GetComponent<CinemachineImpulseSource>();
+    }
+    public void ShakeScreen()
+    {
+        source.GenerateImpulse();
+    }
+    
+}
